cb01761ff81f1250936a9545e47fd6821df10013
joe20182 <joe790924@gmail.com>
2022-06-29 11:01:35 +0800

    add: API_URL

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+const API_URL = "https://api.frontendexpert.io/api/fe/wordle-words";
 
 function App() {
   return (
