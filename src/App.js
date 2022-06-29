3bf4ca87e5237395de2e06a5f5bf99e41dfa5c1f
Joe <joe790924@icloud.com>
2022-06-29 13:49:34 +0800

    edit: style

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { WORDS } from "./utils/words";
 
 function App() {
   const [puzzle, setPuzzle] = useState("");
+  const [words, setWords] = useState(Array(6).fill(""));
 
   useEffect(() => {
     const getWord = async () => {
@@ -20,6 +21,23 @@ function App() {
   return (
     <div className="App">
       <span>{puzzle}</span>
+      <div className="container">
+        {words.map((word, i) => (
+          <Row key={i} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+function Row() {
+  return (
+    <div className="row">
+      <div className="letter">1</div>
+      <div className="letter">2</div>
+      <div className="letter">3</div>
+      <div className="letter">4</div>
+      <div className="letter">5</div>
     </div>
   );
 }
