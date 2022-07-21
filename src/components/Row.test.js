db4c8314a0dc684ebe0db9d9331b756d7d7a26f8
joe20182 <joe790924@gmail.com>
2022-07-21 17:19:43 +0800

    edit: test case

--- src/components/Row.test.js
+++ src/components/Row.test.js
@@ -2,10 +2,33 @@ import { render, screen } from "@testing-library/react";
 import Row from "./Row";
 
 const puzzle = "RAISE";
-const word = "ABCDE";
 
-test("render 1st letter", () => {
+describe("render words", () => {
+  const word = "ABCDE";
   render(<Row word={word} puzzle={puzzle} checked={false} />);
-  const a = screen.getByText("A");
-  expect(a).toBeInTheDocument();
+
+  test("render 1st letter", () => {
+    const ele = screen.getByText(word[0]);
+    expect(ele).toBeInTheDocument();
+  });
+
+  // test("render 2nd letter", () => {
+  //   const ele = screen.getByText(word[1]);
+  //   expect(ele).toBeInTheDocument();
+  // });
+
+  // test("render 3rd letter", () => {
+  //   const ele = screen.getByText(word[2]);
+  //   expect(ele).toBeInTheDocument();
+  // });
+
+  // test("render 4th letter", () => {
+  //   const ele = screen.getByText(word[3]);
+  //   expect(ele).toBeInTheDocument();
+  // });
+
+  // test("render 5th letter", () => {
+  //   const ele = screen.getByText(word[4]);
+  //   expect(ele).toBeInTheDocument();
+  // });
 });
